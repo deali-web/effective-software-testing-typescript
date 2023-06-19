730f75e70378209cdddd00fbacfe050513e3b0f8
오다혜 <fffvvv6@gmail.com>
2023-06-20 00:24:13 +0900

    test: c8 대신 v8 로 coverage 툴 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
 	test: {
 		globals: true,
 		coverage: {
-			provider: 'c8',
+			provider: 'v8',
+			exclude: ['.pnp.*'],
 		},
 		reporters: ['default', 'html'],
 	},
