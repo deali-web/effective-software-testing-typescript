c9db7fd3de3c3ed31f18d3c36ca0ccadf2c69e62
joojaewoo <wnwodn4293@naver.com>
2023-06-19 15:48:44 +0900

    refactor: pr리뷰 반영

--- src/ch3/main/count-words.ts
+++ src/ch3/main/count-words.ts
@@ -1,14 +1,17 @@
+const isLetter = (str: string) =>
+	str.length === 1 && str.toLowerCase() !== str.toUpperCase();
+
 const countWords = () => {
 	const count = (str: string) => {
 		let words = 0;
 		let last = ' ';
 		for (let i = 0; i < str.length; i++) {
-			if (isNaN(+str.charAt(i)) && (last === 's' || last === 'r')) {
+			if (isLetter(str.charAt(i)) && (last === 's' || last === 'r')) {
 				words++;
 			}
 			last = str.charAt(i);
 		}
-		if (last === 'r' || last === 'r') words++;
+		if (last === 's' || last === 'r') words++;
 		return words;
 	};
 	return { count };
